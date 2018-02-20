aba3a8a844dba51908366416e6cbfa27b7da1876
mmr.K <lmimsra07@gmail.com>
2018-02-20 22:03:47 +0900

    add web

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,20 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Http\Request;
+use app\Post;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
 
+Route::post('/send',function (Request $request){
+
+});
+
+Route::delete('post/{post}',function (Post $post){
+
+});
+
+//面白半分で書いた
 Route::get('welcome','WelcomeController@welcome');
