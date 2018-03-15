819fc290c243bc9fd176fb7b0a9e54048f4e851f
mmr.K <lmimsra07@gmail.com>
2018-03-15 11:13:46 +0900

    update thml

--- resources/views/layout/poster.blade.php
+++ resources/views/layout/poster.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Book List</title>
+    <title>Post List</title>
     <!-- CSS と JavaScript -->
 </head>
 <body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,2 +1,27 @@
 @extends('layout.poster')
 @section('content')
+    <div class="panel-body">
+        {{--バリデーションエラーで使用する--}}
+        @include('common.errors')
+
+        {{--登録フォーム--}}
+        <form action="{{url('posts')}}" method="post" class="form-horizontal">
+            {{csrf_field()}}
+            {{--投稿のタイトル--}}
+            <div class="form-group">
+                <label for="post" class="col-sm-3 custom-control-label">Post</label>
+                <div class="col-sm-6">
+                    <input type="text" name="item_name" id="post-name" class="form-control">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="glyphicon glyphicon-plus"></i> Save
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
