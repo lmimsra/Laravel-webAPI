7de4d8d5792a9daecb42dcab2d7b77c918446e78
mmr.K <lmimsra07@gmail.com>
2018-03-16 14:26:36 +0900

    update error.blade.php

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,14 @@
+<!-- error 画面 -->
+
+@if(count($errors) > 0)
+    <div class="alert alert-danger">
+        <div><strong>入力した文字を修正してください</strong></div>
+        <div>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+@endif
